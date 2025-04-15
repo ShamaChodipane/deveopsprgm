b3f2ee0d92a47af990b0dcf6f1f260e253e42bb7
ShamaChodiapne <shamachodipane@gmail.com>
2025-04-15 15:04:03 +0530

    azure pipeline example

---
+++ src/main/java/com/example/LAB2/_4/Lab224Application.java
@@ -0,0 +1,13 @@
+package com.example.LAB2._4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab224Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab224Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LAB2/_4/Lab224ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LAB2._4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab224ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
